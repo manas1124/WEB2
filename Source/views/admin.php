2dcec92a12e380d8557e957d2e77d061debabad8
quang <121563086+loimaxto@users.noreply.github.com>
2025-03-20 08:36:51 +0700

    test

---
+++ Source/views/admin.php
@@ -0,0 +1,6 @@
+<h1>Admin Dashboard</h1>
+<nav>
+    <a href="/admin/home" data-route="/admin/home">Admin Home</a>
+    <a href="/admin/test" data-route="/admin/test">Admin Test</a>
+</nav>
+<div id="admin-content"></div>
