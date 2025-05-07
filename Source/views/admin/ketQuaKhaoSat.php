c7f046c4a2b7559ff0cb68c671582d7cf8dda27f
manas1124 <khanhday1124@gmail.com>
2025-05-07 19:37:31 +0700

    admin: ket qua khao sat

--- Source/views/admin/ketQuaKhaoSat.php
+++ Source/views/admin/ketQuaKhaoSat.php
@@ -3,44 +3,49 @@
         <h1>Kết quả khảo sát</h1>
     </div>
     <div class="flex items-center justify-between mb-4">
-        <div>
-            <label class="label-text" for="select-status">Status </label>
-            <input type="text">
+        <div class="flex items-center gap-4">
+            <div>
+                <label class="label-text mb-1">Status</label>
+                <input type="text" class="input w-48" />
+            </div>
+            <div>
+                <label class="label-text" for=""></label>
+                <button class="btn btn-primary h-10">Lọc</button>
+            </div>
         </div>
     </div>
     <div class="flex items-center justify-between mb-4">
         <div class="flex items-center gap-4">
             <div>
-                <label class="label-text" for="select-status">Status </label>
-                <select class="select" id="select-status">
+                <label class="label-text" for="">Status </label>
+                <select class="select" id="">
                     <option value="-1">Chọn status</option>
                 </select>
             </div>
             <div>
-                <label class="label-text" for="select-status">Status </label>
-                <select class="select" id="select-status">
+                <label class="label-text" for="">ngay </label>
+                <input type="date" class="input" name="" id="">
+            </div>
+            <div>
+                <label class="label-text" for="">ngay </label>
+                <input type="date" class="input" name="" id="">
+            </div>
+            <div>
+                <label class="label-text" for="">Status </label>
+                <select class="select" id="">
                     <option value="-1">Chọn status</option>
                 </select>
             </div>
             <div>
-                <label class="label-text" for="select-status">Status </label>
-                <select class="select" id="select-status">
+                <label class="label-text" for="">Status </label>
+                <select class="select" id="">
                     <option value="-1">Chọn status</option>
                 </select>
             </div>
             <div>
-                <label class="label-text" for="select-status">Status </label>
-                <select class="select" id="select-status">
-                    <option value="-1">Chọn status</option>
-                </select>
+                <label class="label-text" for=""></label>
+                <button class="btn btn-primary" data-act="kqks-loc">Lọc</button>
             </div>
-            <div>
-                <label class="label-text" for="select-status">Status </label>
-                <select class="select" id="select-status">
-                    <option value="-1">Chọn status</option>
-                </select>
-            </div>
-            <button class="btn btn-primary" data-act="kqks-loc">Lọc</button>
         </div>
     </div>
     <div class="w-full overflow-x-auto">
@@ -57,7 +62,7 @@
                 </tr>
             </thead>
             <tbody id="ks-list">
-                
+
             </tbody>
         </table>
     </div>
