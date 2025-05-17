e46d5a14eb0ad7ff5d7ff2acd6cd19e0a16da097
manas1124 <khanhday1124@gmail.com>
2025-05-17 09:56:36 +0700

    hiển thị bài khảo sát của user

--- Source/views/user/survey.php
+++ Source/views/user/survey.php
@@ -1,18 +1,28 @@
 <?php
     require_once __DIR__ .'/../../models/SurveyModel.php';
     require_once __DIR__ .'/../../models/AccountModel.php';
+    require_once __DIR__ .'/../../models/UserModel.php';
     require_once __DIR__ . '/../../utils/JwtUtil.php';
     session_start();
+    $doiTuong = null;
     $doiTuongId = null;
     if (isset($_SESSION['accessToken']) && $_SESSION['accessToken']) {
-                $accessToken = $_SESSION['accessToken'];
-                $object = validateToken($accessToken);
-                $doiTuongId = $object->dtId;
-                $isVaid = isAuthorization($accessToken, 'view.survey');
-                if(!$isVaid) {
-                    echo 'Ko có quyền truy cập!';
-                    exit;
-                }
+        $accessToken = $_SESSION['accessToken'];
+        $object = validateToken($accessToken);
+        $doiTuongId = $object->dtId;
+        $isVaid = isAuthorization($accessToken, 'view.survey');
+        if(!$isVaid) {
+            echo 'Ko có quyền truy cập!';
+            exit;
+        }
+        if($doiTuongId){
+            $userModel = new UserModel();
+            $doiTuong = $userModel->getUserById($doiTuongId);
+            if(!$doiTuong){
+                echo 'Không tìm thấy thông tin của bạn';
+                exit;
+            }
+        }
     }
     else {
         echo 'Không có quyền truy cập!';
@@ -32,7 +42,7 @@
     $listSurvey = json_decode($surveyModel->getAllSurveys(), true);
     echo '<div class="min-h-[608px] w-full grid grid-cols-4 gap-4 p-5 -z-1">';
     foreach($listSurvey as $survey) { 
-        if($survey['su_dung'] == 0 || $survey['su_dung'] == 2){
+        if($survey['su_dung'] == 0 || $survey['su_dung'] == 2 || $doiTuong['nhom_ks'] != $survey['nks_id']){
             continue;
         }
         $isDisabled =disabledBtn($doiTuongId,$survey['ks_id']);
@@ -41,7 +51,7 @@
                         <h5 class="card-title">'. $survey['ten_ks'] .'</h5>
                     </div>
                     <div class="card-body">
-                        <p>Nhóm khảo sát: '. $survey['nks_id'] .'</p>
+                        <p>Nhóm khảo sát: '. $survey['ten_nks'] .'</p>
                         <p>Ngày bắt đầu: '. $survey['ngay_bat_dau'] .'</p>
                         <p>Ngày Kết thúc: '. $survey['ngay_ket_thuc'] . '</p>
 
